15ba376cfec641fde074e1414822ec546b89be19
Hans Sung <lebron990807@gmail.com>
2026-04-28 00:10:46 -0700

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ''
+        for first_word_index, first_word_slice in enumerate(strs[0]):
+            for each_word in strs:
+                if first_word_index >= len(each_word) or first_word_slice != each_word[first_word_index]:
+                    return res
+            res += first_word_slice
+        return res
