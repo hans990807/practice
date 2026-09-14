9c5fc7ea702904ce983c7afdff5592dd843c418c
Hans Sung <lebron990807@gmail.com>
2026-09-13 18:17:49 -0700

    Add: majority-element - submission-9

---
+++ Data Structures & Algorithms/majority-element/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # # hashmap, count and return when > n/2
+        # count = collections.defaultdict(int)
+        # for num in nums:
+        #     count[num] += 1
+        #     if count[num] > len(nums) / 2:
+        #         return num
+        
+        # sort, return the mid num
+        sorted_nums = sorted(nums)
+        return sorted_nums[len(nums) // 2]
